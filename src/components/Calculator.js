95cf01d289f118e96c1e1e969d1c57f34eee457e
Brack <Bradack.Barnard@gmail.com>
2018-10-02 21:46:27 -0600

    monster data for first region

---
+++ src/components/Calculator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Calculator extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Calculator
---
+++ src/data/monsters.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    name: 'Lizardman Archer',
+  },
+  {
+    name: 'Lizardman Shaman',
+  },
+  {
+    name: 'Lizardman Warrior',
+  },
+  {
+    name: 'Wolf Cub',
+    family: 'Wolf Cub',
+  },
+  {
+    name: 'Wolf',
+    family: 'Wolf Cub',
+  },
+  {
+    name: 'Silverback',
+    family: 'Wolf Cub',
+  },
+  {
+    name: 'Arcane Bird',
+    family: 'Arcane Bird'
+  },
+  {
+    name: 'Arcane Eagle',
+    family: 'Arcane Bird'
+  },
+  {
+    name: 'Frost Simurgh',
+    family: 'Arcane Bird',
+    evolved: 'Simurgh',
+  },
+  {
+    name: 'Harpy',
+    family: 'Harpy'
+  },
+  {
+    name: 'Fury',
+    family: 'Harpy'
+  },
+  {
+    name: 'Dragon',
+  }
+]
